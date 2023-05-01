9f4b54fc7b934d131a7daccfa20c62a96fb0ddc0
AlexisMartínez <ia.alexismartinez@ufg.edu.sv>
2023-05-01 13:19:37 -0600

    Fix editorial model

--- models/editorial.py
+++ models/editorial.py
@@ -1,26 +1,8 @@
+from datetime import date
+
 class Editorial:
-    def __init__(self, idEditorial, nameEditorial, dateAdd):
-        self.idEditorial = idEditorial
-        self.nameEditorial = nameEditorial
-        self.dateAdd = dateAdd
 
-    def getId(self):
-        return self.idEditorial
-
-    def getName(self):
-        return self.nameEditorial
-
-    def getDateAdd(self):
-        return self.dateAdd
-
-    def setId(self, idEditorial):
-        self.idEditorial = idEditorial
-
-    def setName(self, nameEditorial):
-        self.nameEditorial = nameEditorial
-
-    def setDateAdd(self, dateAdd):
-        self.dateAdd = dateAdd
-
-    def __str__(self):
-        return f"Editorial({self.idEditorial}, {self.nameEditorial}, {self.dateAdd})"
+    idEditorial: int = 0
+    nameEditorial: str = ""
+    dateAdd: date = date(1900, 1, 1)
+    statusEditorial: bool = True
