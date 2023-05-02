8580445efbdfba3258931e340b9ee71b3a061439
Alfredo Alas <alfredo.alas14@gmail.com>
2023-05-02 11:28:26 -0600

    Adds User class

--- models/user.py
+++ models/user.py
@@ -1,40 +1,14 @@
-class User:
-    def __init__(self, idUser, nameUser, carnetUser, emailUser, phoneUser):
-        self.idUser = idUser
-        self.nameUser = nameUser
-        self.carnetUser = carnetUser
-        self.emailUser = emailUser
-        self.phoneUser = phoneUser
+#Imports
+from pydantic import BaseModel
+from datetime import date
+from typing import Optional
 
-    def getIdUser(self):
-        return self.idUser
+#Create model to User object
+class User(BaseModel):
 
-    def getNameUser(self):
-        return self.nameUser
-
-    def getCarnetUser(self):
-        return self.carnetUser
-
-    def getEmailUser(self):
-        return self.emailUser
-
-    def getPhoneUser(self):
-        return self.phoneUser
-
-    def setIdUser(self, idUser):
-        self.idUser = idUser
-
-    def setNameUser(self, nameUser):
-        self.nameUser = nameUser
-
-    def setCarnetUser(self, carnetUser):
-        self.carnetUser = carnetUser
-
-    def setEmailUser(self, emailUser):
-        self.emailUser = emailUser
-
-    def setPhoneUser(self, phoneUser):
-        self.phoneUser = phoneUser
-
-    def __str__(self):
-        return f"User({self.idUser}, {self.nameUser}, {self.carnetUser}, {self.emailUser}, {self.phoneUser})"
+    idUser: int = 0
+    nameUser: str = ""
+    carnetUser: str = ""
+    emailUser: str = ""
+    phoneUser: str = ""
+    statusUser: bool = True
